b8760d1caa17befac52134a67b61e2da45f3093f
jcastro-ec <64545499+jcastro-ec@users.noreply.github.com>
2020-04-29 18:26:25 +0200

    bin added

---
+++ bin/web/connectdialog.js
@@ -0,0 +1,153 @@
+﻿define( ['qvangular',
+		'text!QvGamsConnector.webroot/connectdialog.ng.html',
+		'css!QvGamsConnector.webroot/connectdialog.css'
+], function ( qvangular, template) {
+	return {
+		template: template,
+		controller: ['$scope', 'input', function ( $scope, input ) {
+			function init() {
+				$scope.isLoading = true;
+			
+				$scope.isEdit = input.editMode;
+				$scope.id = input.instanceId;
+				$scope.titleText = $scope.isEdit ? "Modify connection (GAMS)" : "Create new connection (GAMS)";
+				$scope.saveButtonText = $scope.isEdit ? "Update" : "Create";
+
+				$scope.name = "";
+				$scope.username = "Username";
+				$scope.password = "Password";
+				$scope.provider = "QvGamsConnector.exe"; // Connector filename
+				$scope.filesourcepath = "";
+				$scope.connectionInfo = "";
+				$scope.connectionSuccessful = false;
+				$scope.connectionString = createCustomConnectionString($scope.provider, "");
+				
+				$scope.folders = [];
+				$scope.files = [];
+				$scope.pathItems = [];
+
+				input.serverside.sendJsonRequest( "getInfo" ).then( function ( info ) {
+					$scope.info = info.qMessage;
+				} );
+
+				if ( $scope.isEdit ) {
+					input.serverside.getConnection( $scope.id ).then( function ( result ) {
+						$scope.name = result.qConnection.qName;	
+						$scope.username = result.qConnection.username;
+						$scope.password = result.qConnection.password;
+						$scope.connectionString = result.qConnection.qConnectionString;
+						
+						input.serverside.sendJsonRequest("getConnectionInfo", result.qConnection.qConnectionString, 'true' /*include credentials*/).then(function (connectionInfoResponse) {
+							$scope.filesourcepath = connectionInfoResponse.qMessage;
+							getServerFolders("");
+						});
+						$scope.connectionString = createCustomConnectionString($scope.provider, result.qConnection.qfilesourcepath);
+						
+						$scope.password = result.qConnection.qMessage;
+					} );
+				} else {
+					getServerFolders("");
+				}
+			}
+
+
+
+			$scope.onTestConnectionClicked = function () {
+				$scope.isLoading = true;
+				input.serverside.sendJsonRequest( "testConnection", $scope.filesourcepath ).then( function ( info ) {
+					$scope.connectionInfo = info.qMessage;
+					$scope.connectionSuccessful = info.qMessage.indexOf( "OK" ) !== -1;
+					$scope.connectionString = createCustomConnectionString($scope.provider, $scope.filesourcepath);
+					$scope.isLoading = false;
+				},
+				function ( error ) {
+					console.error(error);
+					$scope.isLoading = false;
+				});
+			};
+
+			$scope.isOkEnabled = function () {
+				return $scope.name.length > 0 && $scope.connectionSuccessful;
+			};
+
+			$scope.onEscape = $scope.onCancelClicked = function () {
+				$scope.destroyComponent();
+			};
+
+			$scope.onOKClicked = function () {
+				console.info("Estamos en la segunda OkCliked");
+				if($scope.isEdit) {
+					console.info("Estamos en la segunda OkCliked y isEdit is enabled");
+					console.info($scope);
+						input.serverside.modifyConnection($scope.id, $scope.name, $scope.connectionString, $scope.provider, true, $scope.username, $scope.password).then(function(result){							
+						if(result){
+							$scope.destroyComponent();
+						}
+					},
+					function ( error ) {
+						console.error(error);
+					});
+				}
+				else {
+					input.serverside.createNewConnection($scope.name, $scope.connectionString, $scope.username, $scope.password, $scope.filesourcepath).then(function(result){
+						if(result){
+							$scope.destroyComponent();
+						}
+					},
+					function ( error ) {
+						console.error(error);
+					});
+					
+				}
+			};
+			
+			$scope.getServerFolders = function(folder) {
+				getServerFolders(folder);
+			};
+			
+			$scope.goToPath = function(path) {
+				$scope.filesourcepath = path;
+				getServerFolders("");
+			};
+
+			
+			/* Helper functions */
+
+
+			function createCustomConnectionString ( filename, connectionstring ) {
+				//console.info("createCustomString:"+connectionstring)
+				if (connectionstring != "")
+				{
+				return "CUSTOM CONNECT TO " + "\"provider=" + filename + ";source=" +  connectionstring + ";\"";
+				
+				} else {
+					return "CUSTOM CONNECT TO " + "\"provider=" + filename + ";\"";			
+				}				
+			}
+
+			function createConnectionString ( filename, connectionstring ) {
+				
+				return "CUSTOM CONNECT TO " + "\"provider=" + filename + ";\"";
+			}
+			
+			function getServerFolders (selectedFolder) {
+				$scope.isLoading = true;
+				input.serverside.sendJsonRequest( "getServerFolders", $scope.filesourcepath, selectedFolder ).then( function ( serverFolderInfo ) {
+					$scope.filesourcepath = serverFolderInfo.qCurrentPath;
+					$scope.folders = serverFolderInfo.qFolders;
+					$scope.files = serverFolderInfo.qFiles;
+					$scope.pathItems = serverFolderInfo.qCurrentPathList;
+					
+					$scope.isLoading = false;
+					$(".browserDiv")[0].scrollTo(0,0);
+				},
+				function ( error ) {
+					console.error(error);
+					$scope.isLoading = false;
+				});
+			}
+
+			init();
+		}]
+	};
+} );
---
+++ bin/web/selectdialog.js
@@ -0,0 +1,271 @@
+﻿define( ['qvangular',
+"underscore"
+], function ( qvangular, underscore ) {
+	return ['serverside', 'standardSelectDialogService', function ( serverside, standardSelectDialogService ) {
+		var eventlogDialogContentProvider = {
+			getConnectionInfo: function () {
+				var scopeOptions = $("#selectAllFields").scope().options;
+				scopeOptions.databaseTitle = 'Folder';
+				scopeOptions.databasePlaceholder = 'Select a folder...';
+				scopeOptions.ownerTitle = 'File';
+				scopeOptions.ownerPlaceholder = 'Select a file...';
+				scopeOptions.tablesTitle = 'Symbols';
+				
+				return qvangular.promise( {
+					dbusage: false,
+					ownerusage: false,
+					dbseparator: '.',
+					ownerseparator: '.',
+					specialchars: '! "$&\'()*+,-/:;<>`{}~[]',
+					quotesuffix: '"',
+					quoteprefix: '"',
+					dbfirst: true,
+					keywords: []
+				} );
+			},
+			getDatabases: function () {				
+				return serverside.sendJsonRequest( "getDatabases" ).then( function ( response ) {
+					return response.qDatabases;
+				} );
+			},
+			getOwners: function (qDatabaseName) {
+				return serverside.sendJsonRequest( "getOwners", qDatabaseName ).then( function ( response ) {
+					return response.qOwners;
+				} );
+			},
+			getTables: function ( qDatabaseName, qOwnerName ) {
+				return serverside.sendJsonRequest( "getTables", qDatabaseName, qOwnerName ).then( function ( response ) {
+					return response.qTables;
+				} );
+			},
+			getFields: function ( qDatabaseName, qOwnerName, qTableName ) {
+				return serverside.sendJsonRequest( "getFields", qDatabaseName, qOwnerName, qTableName ).then( function ( response ) {
+					return response.qFields;
+				} );
+			},
+			getPreview: function( qDatabaseName, qOwnerName, qTableName ) {
+				return serverside.sendJsonRequest("getPreview", qDatabaseName, qOwnerName, qTableName).then(function (response) {				
+				   return qvangular.promise(response.qPreview);				
+				} );
+				
+			}
+		};
+		
+		function filterSelected(list) {
+			return underscore.filter(list, function (x) {
+				return x.checked;
+			});
+		}
+
+		function atLeastOneFieldHasAlias(selectedFields) {
+			for (var field in selectedFields) {
+				if (selectedFields[field].alias && selectedFields[field].alias !== selectedFields[field].name) {
+					return true;
+				}
+			}
+			return false;
+		}
+
+		function fieldSelectionChanged(newFields, existingFields) {
+			if (newFields.fields.length != existingFields.fields.length) {
+				return true;
+			}
+
+			// if any field is not found in one list, the selections are different
+			for (var listIndex in newFields.fields) {
+				if (newFields.fields[listIndex].identifierName != existingFields.fields[listIndex].identifierName || newFields.fields[listIndex].aliasName != existingFields.fields[listIndex].aliasName) {
+					return true;
+				}
+			}
+
+			return false;
+		}
+
+		function tableLoadPropertiesChanged(newLoadProperties, oldLoadProperties) {
+		    if(!newLoadProperties && !oldLoadProperties) {
+		        return false;
+		    }
+		    if(!newLoadProperties || !oldLoadProperties) {
+		        return true;    // only one can be null so they are different
+		    }
+
+            // filter checks
+		    if(!newLoadProperties.filterInfo && !oldLoadProperties.filterInfo) {
+		        return false;
+		    }
+
+		    if(!newLoadProperties.filterInfo || !oldLoadProperties.filterInfo) {
+		        return true;
+		    }
+
+		    if(newLoadProperties.filterInfo.filterType !== oldLoadProperties.filterInfo.filterType) {
+		        return true;
+		    }
+		    
+		    if(newLoadProperties.filterInfo.filterClause !== oldLoadProperties.filterInfo.selectGeneratedFilterClause) {
+		        return true;
+		    }
+
+		    return false;
+		}
+		
+		function isAddScriptButtonDisabled() {
+			return getCurrentScript().trim() == "";
+		}
+		
+		function createQueryBuilderInfo(selectedFields, precedingLoad, tableLoadProperties) {
+			var queryBuilderObject = {};
+
+			var table = selectedFields[0].table;
+			queryBuilderObject.tableName = table.name;
+			queryBuilderObject.ownerName = table.ownerName;
+			queryBuilderObject.databaseName = table.databaseName;
+
+			//queryBuilderObject.tableQualifiers = [];
+			//queryBuilderObject.tableQualifiers.push(table.databaseName);
+			//queryBuilderObject.tableQualifiers.push(table.ownerName);
+
+			queryBuilderObject.key = ((table.databaseName || "") != "" ? table.databaseName + "." : "") + 
+										((table.ownerName || "") != "" ? table.ownerName + "." : "") + 
+										table.name;
+
+			queryBuilderObject.fields = [];
+
+			selectedFields.forEach(function (field) {
+				var aliasName = field.name !== field.alias ? field.alias : "";
+
+				queryBuilderObject.fields.push( { displayName: field.name, identifierName: field.identifierName, aliasName: aliasName });
+			});
+
+			queryBuilderObject.includePrecedingLoad = precedingLoad;
+			queryBuilderObject.loadProperties = tableLoadProperties;
+
+			return queryBuilderObject;
+		}
+		
+		function getCurrentScript() {
+			var script = "";
+
+			for (var index in currentQueryBuilderInfoList) {
+				var queryInfo = currentQueryBuilderInfoList[index];
+
+				//if there was error during select script load process or not all data are loaded return empty script
+				if (queryInfo.hasOwnProperty("success") && queryInfo.success != true ||  queryInfo.isLoading === true)
+					return "";
+
+				script += queryInfo.script;
+				script += "\r\n\r\n";
+			}
+
+			return script.trim();
+		}
+		
+		var fieldsListCache = [];
+		var includeLoadStatementState = null;
+
+		function generateSelectScript(datasourceSelectionModel, datasourceInfo, precedingLoad, selectAll) {
+			var databases, selectedFields;
+			var updatedQueryBuilderInfoList = [];
+
+			if (datasourceInfo) {
+				//script += datasourceInfo.connectString + "\r\n";
+				databases = datasourceSelectionModel.databases;
+				if (databases) {
+
+					var allTables = $.map(databases, function (database) {
+						var dbOwners = database.owners;
+						if (dbOwners && dbOwners.length > 0) {
+							return $.map(dbOwners,function(owner) {
+								if (owner.tables && owner.tables.length > 0) {
+									return owner.tables;
+								}
+							});
+						}
+					});
+
+					//filter tables that contain any selected fields
+					var tablesFields=$.map(allTables,function(table) {
+						selectedFields = filterSelected(table.fields);
+						if (selectedFields && selectedFields.length > 0) {
+							return { 
+								key:table.ownerName + "_" + table.databaseName+ "_" +	table.name,
+								fields: selectedFields,
+								loadProperties: table.loadProperties
+						};
+						}
+					});
+
+					//check is current tables list allready loaded to prevent "currentQueryBuilderInfoList" clearence
+					var tablesAreSame=underscore.isEqual(
+						underscore.sortBy(fieldsListCache, function(table) {return table.key;}),
+						underscore.sortBy(tablesFields, function(table) { return table.key;})
+					);
+					if (tablesAreSame && includeLoadStatementState === precedingLoad)
+						return getCurrentScript();
+
+					tablesFields.forEach(function (table) {
+						var fields = table.fields;
+						var queryBuilderInfo = createQueryBuilderInfo(fields, precedingLoad || atLeastOneFieldHasAlias(fields), table.loadProperties ); //, selectAll
+
+						// check if this table is already in the list
+						var currentQueryBuilderInfo = underscore.find(currentQueryBuilderInfoList, function(info) { return info.key == queryBuilderInfo.key; });
+
+						if (currentQueryBuilderInfo === undefined || queryBuilderInfo.tableName != currentQueryBuilderInfo.tableName || queryBuilderInfo.includePrecedingLoad != currentQueryBuilderInfo.includePrecedingLoad || fieldSelectionChanged(queryBuilderInfo, currentQueryBuilderInfo) || tableLoadPropertiesChanged(queryBuilderInfo.loadProperties, currentQueryBuilderInfo.loadProperties)) {
+							currentQueryBuilderInfo = queryBuilderInfo;
+							currentQueryBuilderInfo.script = ""; // Clear out the script since it will be retrieved from the connector
+							currentQueryBuilderInfo.isLoading = true; //setting "table is loading" flag
+							//currentQueryBuilderInfo.loadProperties.filterInfo.selectGeneratedFilterClause = currentQueryBuilderInfo.loadProperties.filterInfo.filterClause; // copy the state of filter when select is generated because it can change often as the user types
+							var queryBuilderModelJson = queryBuilderInfo;
+
+							serverside.sendJsonRequest("getSelectScript", queryBuilderModelJson).then(function(responseScript) {
+								currentQueryBuilderInfo.success = true;
+								currentQueryBuilderInfo.script = responseScript.query;	
+								currentQueryBuilderInfo.isLoading = false;
+							});
+							
+							//dataSelectionModel.getSelectScript(queryBuilderModelJson, []).then(function(responseScript) {
+
+							//	currentQueryBuilderInfo.success = responseScript.success;
+
+								//serverside.sendJsonRequest("getSelectScript", queryBuilderModelJson).then(function(responseScript) {
+								//for (var index in responseScript.generatedScriptList) {
+								//	var generatedScriptInfo = responseScript.generatedScriptList[index];
+                                //
+								//	currentQueryBuilderInfo.script = generatedScriptInfo.script;
+								//	currentQueryBuilderInfo.isLoading = false; //table is loaded
+								//}
+								
+							//});
+						} else {
+							//script = currentQueryText;
+
+						}
+
+						// Add the new or existing item to the list
+						updatedQueryBuilderInfoList.push(currentQueryBuilderInfo);
+					});
+					//update loaded tables list. fieldsListCache can be differ from "updatedQueryBuilderInfoList" if several "generateSelectScript" executed simultaneously
+					fieldsListCache = tablesFields;
+					includeLoadStatementState === precedingLoad;
+				}
+
+				// Update the list
+				currentQueryBuilderInfoList = updatedQueryBuilderInfoList;
+
+				// Generate the script for all selections
+				var script = getCurrentScript();
+				
+				return script;
+			} else {
+				return "";
+			}			
+		}
+
+		standardSelectDialogService.showStandardDialog( eventlogDialogContentProvider, {
+			precedingLoadVisible: true,
+			fieldsAreSelectable: true,
+			allowFieldRename: true,
+			scriptGenerator: { generateScriptForSelections: generateSelectScript }
+		});
+	}];
+} );
